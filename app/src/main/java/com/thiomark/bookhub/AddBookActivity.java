8d9c874377c254c27d2d5cd88a9331ee78341057
SINELIHLE <sinelihlengqaza@gmail.com>
2022-05-03 19:14:42 -0700

    ádd image of book

--- app/src/main/java/com/thiomark/bookhub/AddBookActivity.java
+++ app/src/main/java/com/thiomark/bookhub/AddBookActivity.java
@@ -1,18 +1,33 @@
 package com.thiomark.bookhub;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class AddBookActivity extends AppCompatActivity {
 
+    private static final int  PERMISSION_CODE = 1000;
+    private static final int  IMAGE_CAPTURE_CODE = 1001;
     private EditText bookTitle, bookDescription, currentPage;
     private Button btn;
     String title, description, currPage;
+    ImageButton image;
+
+    Uri img_uri;
 
    public static final String EXTRA_TEXT = "com.thiomark.bookhub.EXTRA_TEXT";
 
@@ -24,7 +39,34 @@ public class AddBookActivity extends AppCompatActivity {
         bookTitle = findViewById(R.id.title_txt);
         bookDescription = findViewById(R.id.description_txt);
         currentPage = findViewById(R.id.current_page_txt);
+        image = findViewById(R.id.add_image_btn);
 
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)==
+                                PackageManager.PERMISSION_DENIED){
+                        //permission denied
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+                        //show request message
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+                    else{
+                        //if permission granted already
+                        openCamera();
+                    }
+                }
+                else{
+                    //os is less than required
+                    openCamera();
+
+                }
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -41,5 +83,41 @@ public class AddBookActivity extends AppCompatActivity {
         });
     }
 
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "NEW PICTURE");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "FROM CAMERA");
+        img_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
+        //camera intent
+        Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        camIntent.putExtra(MediaStore.EXTRA_OUTPUT, img_uri);
+        startActivityForResult(camIntent, IMAGE_CAPTURE_CODE);
+
+    }
+//persion result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case PERMISSION_CODE:{
+                if(grantResults.length > 0 && grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED){
+                    openCamera();
+                }
+                else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            //image captured to image button
+            image.setImageURI(img_uri);
+        }
+    }
 }
